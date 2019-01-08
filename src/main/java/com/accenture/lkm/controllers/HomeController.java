0c96a968a50276874479d3cd1df29764868337ef
ajay.ojha <ajay.ojha@accenture.com>
2019-01-08 10:13:25 +0530

    Modified greeting message

--- src/main/java/com/accenture/lkm/controllers/HomeController.java
+++ src/main/java/com/accenture/lkm/controllers/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 	public ModelAndView home() {
 		ModelAndView mv = new ModelAndView("index");
 		mv.addObject("title", "Accenture - High performance. Delivered");
-		mv.addObject("greeting", "Welcome to Orchestrating with DevOps Training!");
+		mv.addObject("greeting", "Jenkin Successfull!");
 		return mv;
 	}
 	
