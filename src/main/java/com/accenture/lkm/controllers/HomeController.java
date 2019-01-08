cd59e465619f2093fb06459cf639830e9fabdd39
ajay.ojha <ajay.ojha@accenture.com>
2019-01-08 15:19:54 +0530

    change for pipeline 2

--- src/main/java/com/accenture/lkm/controllers/HomeController.java
+++ src/main/java/com/accenture/lkm/controllers/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 	public ModelAndView home() {
 		ModelAndView mv = new ModelAndView("index");
 		mv.addObject("title", "Accenture - High performance. Delivered");
-		mv.addObject("greeting", "Jenkin Successfull!");
+		mv.addObject("greeting", "Demo For Pipeline");
 		return mv;
 	}
 	
